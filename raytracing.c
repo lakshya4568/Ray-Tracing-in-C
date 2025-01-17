fea33527774e8616c55c60533917ed946e174b63
Lakshya Sharma <lakshya4568@gmail.com>
2025-01-17 20:45:13 +0530

    Update rendering parameters to move the shadow circle object  for more visual represnting and add basic anti-aliasing functionality to remove jagged images

--- raytracing.c
+++ raytracing.c
@@ -3,12 +3,12 @@
 #include </opt/homebrew/include/sdl2/SDL.h>
 
 
-#define WIDTH 1000 
-#define HEIGHT 1000
+#define WIDTH 1200
+#define HEIGHT 600
 #define RAYS_NUMBER 200
 #define COLOR_WHITE 0xffffffff
 #define COLOR_BLACK 0x00000000
-#define RAY_COLOR 0xde2508
+#define RAY_COLOR 0xf4d03f
 #define RAY_THICKNESS 2
 
 typedef struct 
@@ -39,6 +39,94 @@ typedef struct
     double x_start, y_start; 
 } Ray;
 
+
+// ...existing code...
+
+/**
+ * @brief Applies a basic box blur to soften edges on the given SDL surface.
+ *
+ * This function analyzes each pixel and its neighbors, computes an average,
+ * and writes the result back. To keep it simple, we handle only RGB888 or ARGB8888 formats here.
+ *
+ * Note: For performance reasons, we'd typically enable/disable surface locking
+ * and handle boundary conditions carefully.
+ *
+ * @param surface The SDL surface on which to apply anti-aliasing.
+ */
+void BasicAntialiasing(SDL_Surface *surface)
+{
+    // Lock surface if needed
+    if (SDL_MUSTLOCK(surface))
+    {
+        if (SDL_LockSurface(surface) < 0)
+        {
+            return;
+        }
+    }
+
+    // Basic dimension lookup
+    int width = surface->w;
+    int height = surface->h;
+
+    Uint32 *pixels = (Uint32 *)surface->pixels;
+    Uint32 *temp = (Uint32 *)malloc(width * height * sizeof(Uint32));
+    if (!temp)
+    {
+        if (SDL_MUSTLOCK(surface))
+            SDL_UnlockSurface(surface);
+        return;
+    }
+
+    // Copy current data to temp
+    memcpy(temp, pixels, width * height * sizeof(Uint32));
+
+    // For each pixel, average with up to 8 neighbors
+    for (int y = 1; y < height - 1; y++)
+    {
+        for (int x = 1; x < width - 1; x++)
+        {
+            // Accumulators for R, G, B
+            int totalR = 0, totalG = 0, totalB = 0;
+
+            // Sample 3x3 region
+            for (int j = -1; j <= 1; j++)
+            {
+                for (int i = -1; i <= 1; i++)
+                {
+                    int idx = (y + j) * width + (x + i);
+                    Uint32 p = temp[idx];
+
+                    // Extract ARGB or RGB
+                    Uint8 r = (p & 0x00FF0000) >> 16;
+                    Uint8 g = (p & 0x0000FF00) >> 8;
+                    Uint8 b = (p & 0x000000FF);
+
+                    totalR += r;
+                    totalG += g;
+                    totalB += b;
+                }
+            }
+
+            // Average color
+            Uint8 avgR = (Uint8)(totalR / 9);
+            Uint8 avgG = (Uint8)(totalG / 9);
+            Uint8 avgB = (Uint8)(totalB / 9);
+
+            // Write to surface
+            Uint32 finalColor = (0xFF << 24) | (avgR << 16) | (avgG << 8) | (avgB);
+            pixels[y * width + x] = finalColor;
+        }
+    }
+
+    free(temp);
+
+    // Unlock surface if we locked it
+    if (SDL_MUSTLOCK(surface))
+    {
+        SDL_UnlockSurface(surface);
+    }
+}
+
 /**
  * @brief Generates an array of rays originating from a given circle.
  *
@@ -58,6 +146,19 @@ void generate_rays(Circle circle, Ray rays[RAYS_NUMBER]) {
     }
 }
 
+/**
+ * @brief Renders rays on an SDL surface and checks for intersections with a given circle.
+ *
+ * This function takes an array of rays and renders them on the provided SDL surface.
+ * Each ray is drawn from its starting point in the direction specified by its angle.
+ * The function also checks if the rays intersect with a given circle (object) and stops
+ * drawing the ray if an intersection is detected or if the ray reaches the edge of the screen.
+ *
+ * @param surface The SDL surface on which the rays will be drawn.
+ * @param rays An array of rays to be rendered. The size of the array should be RAYS_NUMBER.
+ * @param color The color used to draw the rays.
+ * @param Object The circle object to check for ray intersections.
+ */
 void FillRays(SDL_Surface *surface, Ray rays[RAYS_NUMBER], Uint32 color, Circle Object) {
 
     double radius_squared = pow(Object.r, 2);
@@ -136,11 +237,22 @@ int main()
         SDL_WINDOWPOS_CENTERED, // Initial y position: centered on screen
         WIDTH,                    // Window width in pixels
         HEIGHT,                    // Window height in pixels
-        SDL_WINDOW_SHOWN        // Window is visible upon creation
+        0                // Window is visible upon creation
     );
 
     // retrieve the surface in case we need to draw anything to the window (a sheet)
     SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+    SDL_Surface *offscreen = SDL_CreateRGBSurface(
+        0,
+        WIDTH * 2,
+        HEIGHT * 2,
+        32,
+        0x00FF0000,
+        0x0000FF00,
+        0x000000FF,
+        0xFF000000
+    );
 
     /* creation of a rectangle in sdl 
      SDL_Rect rect = (SDL_Rect){200, 200, 200, 200};
@@ -148,11 +260,11 @@ int main()
      SDL_FillRect(surface, &rect, COLOR_WHITE);
     */
 
-    Circle circle = {200, 200, 80};
+    Circle circle = {200, 200, 70};
     SDL_Rect erase_rect = (SDL_Rect){0, 0, WIDTH, HEIGHT};
 
     // shadow circle
-    Circle shadow_circle = {650, 300, 120};
+    Circle shadow_circle = {550, 300, 120};
     Ray rays[RAYS_NUMBER];
 
 
@@ -168,6 +280,7 @@ int main()
     SDL_Event event;
     int running = 1;  // Flag to control the main loop; 1 means running
 
+    int speed = 6;
     // Enter the main event loop
     while (running) {
         // Process events in the queue, it will store the value at each updation to the x, y coordinates of circle
@@ -189,9 +302,23 @@ int main()
         // we need to erase the circle from the frame before, with surface color, so that it looks the circle is moving
         SDL_FillRect(surface, &erase_rect, COLOR_BLACK);
         FillCircle(surface, shadow_circle, COLOR_WHITE); // shadow circle
-        FillCircle(surface, circle, COLOR_WHITE);
+        FillCircle(surface, circle, COLOR_WHITE); 
         FillRays(surface, rays, RAY_COLOR, shadow_circle);
-        SDL_UpdateWindowSurface(window); // changes to the surface will update here
+
+        //BasicAntialiasing(surface);
+
+        // calculation about how the y-coordinate of the object should behave
+        // when crossing with the speed and it's change in position w.r.t speed
+        shadow_circle.y += speed;
+        if(shadow_circle.y + shadow_circle.r < 0) {
+            speed = -speed;
+        } 
+
+        if(shadow_circle.y + shadow_circle.r > HEIGHT) {
+            speed = -speed;
+        }
+
+        SDL_UpdateWindowSurface(window); // changes to the surface will update her
         SDL_Delay(16);  // Delay to control frame rate; ~16ms for ~60 FPS
     }
 
